086e1bbb7e2a001c1ae55cb8a9c2ca638df5d4cf
rmoralespar <rmoralespar@uoc.edu>
2023-06-25 18:51:06 +0200

    ch

--- app.js
+++ app.js
@@ -17,12 +17,3 @@ function validate() {
 }
 $("#validate").bind("click", validate);
 
-function init() {
-  var vidDefer = document.getElementsByTagName("iframe");
-  for (var i = 0; i < vidDefer.length; i++) {
-    if (vidDefer[i].getAttribute("data-src")) {
-      vidDefer[i].setAttribute("src", vidDefer[i].getAttribute("data-src"));
-    }
-  }
-}
-window.onload = init;
